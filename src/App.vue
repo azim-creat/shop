14ff60f8cd5779b622d4cc1f004bb85e497e8323
azimov.dev <azimov.dev@gmail.com>
2020-07-29 16:30:39 +0600

    navigation panel

--- src/App.vue
+++ src/App.vue
@@ -1,134 +1,163 @@
 <template>
-  <div class="app">      
-      <div class="main_body">
-       <transition name="moveInUp">
-         <router-view/>
-       </transition>
-      </div>
+  <div class="app">
+    <div class="main_body">
+      <transition name="moveInUp">
+        <router-view />
+      </transition>
+    </div>
 
-
-      <div class="navbar">
-            <div class="nav-item">
-              <router-link to="/" class="navbar-brand"><img src="./assets/navbar_icons/home.svg"><span>Главная</span></router-link>
-            </div>
-            <div class="nav-item">
-              <router-link  to="/categoryes"><img src="./assets/navbar_icons/categoryes.svg"><span>Категории</span></router-link>
-            </div>
-            <div class="nav-item nav-item-basket">
-              <router-link class="nav-link" to="/services"><img src="./assets/navbar_icons/basket.svg"><span></span></router-link>
-            </div>
-            <div class="nav-item">
-             <router-link class="nav-link" to="/search"><img src="./assets/navbar_icons/search.svg"><span>Поиск</span></router-link>
-            </div>
-            <div class="nav-item">
-             <router-link class="nav-link" to="/info"><img src="./assets/navbar_icons/help.svg"><span>Инфо</span></router-link>
-            </div>
+    <div class="navbar">
+      <div class="nav-item">
+        <router-link to="/" class="noSelect">
+          <img src="./assets/navbar_icons/home.svg" />
+          <span>Главная</span>
+        </router-link>
       </div>
+      <div class="nav-item">
+        <router-link to="/categoryes" class="noSelect">
+          <img src="./assets/navbar_icons/categoryes.svg" />
+          <span>Категории</span>
+        </router-link>
+      </div>
+      <div class="nav-item nav-item-basket ">
+        <router-link class="noSelect" to="/services">
+          <img src="./assets/navbar_icons/basket.svg" />
+          <span></span>
+        </router-link>
+      </div>
+      <div class="nav-item">
+        <router-link class="noSelect" to="/search">
+          <img src="./assets/navbar_icons/search.svg" />
+          <span>Поиск</span>
+        </router-link>
+      </div>
+      <div class="nav-item">
+        <router-link class="noSelect" to="/info">
+          <img src="./assets/navbar_icons/help.svg" />
+          <span>Инфо</span>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  data ()
-  {
-    return {
-      
-    }
+  name: "App",
+  data() {
+    return {};
   },
-  methods:
-  {
-    showAlert()
-    {
-      alert(111)
+  methods: {
+    showAlert() {
+      alert(111);
     },
-    showAlert2()
-    {
-      alert(222)
-    }
+    showAlert2() {
+      alert(222);
+    },
   },
-  computed :{
-    FIRST_COMPUTED_VALUE(){
-      return this.$store.state.first_value
-    }
+  computed: {
+    FIRST_COMPUTED_VALUE() {
+      return this.$store.state.first_value;
+    },
   },
 
-  watch:{
-    FIRST_COMPUTED_VALUE(val,old_val){
-      console.log(val,old_val)
-    }
+  watch: {
+    FIRST_COMPUTED_VALUE(val, old_val) {
+      console.log(val, old_val);
+    },
   },
-  mounted(){
+  mounted() {
     // console.log('component mounted');
     // //this.showAlert();
     // //this.showAlert2();
     // console.log(this.FIRST_COMPUTED_VALUE)
     // this.$store.dispatch("SET_TO",  ["first_value", 23455])
-  }
-}
+  },
+};
 </script>
 
 <style>
-  body{
-    margin: 0;
-    padding: 0;
-    font-family: 'Roboto', sans-serif;
-    height: 100vh;
-    width: 100vw;
-    overflow: hidden;
-    box-sizing: border-box;
+body {
+  margin: 0;
+  padding: 0;
+  font-family: "Roboto", sans-serif;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+  box-sizing: border-box;
+}
+.noSelect {
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  -webkit-tap-highlight-color: transparent;
+}
+.noSelect:focus {
+  outline: none !important;
+}
 
-  }
+.app {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  max-height: 100vh;
+  overflow: hidden;
+}
+.main_body {
+  flex: 2;
+  overflow-y: scroll;
+  height: 100px;
+  height: -webkit-fill-available;
+  height: 100%;
+}
+.navbar {
+  flex: 0;
+  max-height: fit-content;
+  height: fit-content;
+  display: flex;
+  justify-content: space-around;
+  border-top: 2px solid #b3b3b3;
+  align-items: center;
+  position: fixed;
+  background-color: #fff;
+  bottom: 0;
+  left: 0;
+  padding: 5px 0;
+}
+.nav-item {
+  padding: 10px;
+  text-align: center;
+}
+.nav-item a {
+  text-decoration: none;
+  font-size: 12px;
+  color: #000;
+  height: fit-content;
+  display: block;
+}
+.nav-item a:visited {
+  color: #000;
+}
 
-  .app{
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-    max-height: 100vh;
-    overflow: hidden;
-  }
-  .main_body{
-    flex: 2;
-    overflow-y: scroll;
-    height: 100px;
-    height: -webkit-fill-available;
-    height: 100%;
-  }
-  .navbar{
-    flex: 0;
-    max-height: fit-content;
-    height: fit-content;
-    display: flex;
-    justify-content: space-around;
-  }
-  .nav-item{
-    padding : 10px;
-    text-align:center;
-    
-  }
-  .nav-item a{
-    text-decoration:none;
-    font-size: 14px;
-    color: #000;
-  }
-  .nav-item a:visited {
-    color: #000;
-  }
+.nav-item img {
+  height: 15px;
+}
 
-  .nav-item img{
-    height : 25px;
-  }
+.nav-item-basket {
+  background: linear-gradient(232.84deg, #ff0099 -162.71%, #ff4d00 163.83%);
+  border-radius: 100px;
+  height: fit-content;
+  padding: 10px 20px;
+}
+.nav-item-basket img {
+  height: 25px;
+}
 
-  .nav-item-basket{
-    background: linear-gradient(232.84deg, #FF0099 -162.71%, #FF4D00 163.83%);
-    border-radius: 100px;
-    height: fit-content;
-    padding: 10px 20px;
-  }
-
-  router-link{
-    display:flex;
-    flex-direction:column;
-  }
-
+router-link {
+  display: flex;
+  flex-direction: column;
+}
 </style>
--- src/components/info.vue
+++ src/components/info.vue
@@ -1,204 +1,244 @@
 <template>
-	<div class="info">
-		<div class="info_container">
-            <div class="info_container__img" :style="{'background-image': `url(.${company_info.company_image})`}"></div>
-            <h3 class="company_name">{{company_info.company_name}}</h3>
-            <h5 class="company_address">{{company_info.company_address}}</h5>
-        </div>
-        <div class="info_btns">
-            <div class="info_container__btn">
-                <a class="info_container__numb"><i class="fa 2x fa-phone" style="transform:rotate(90deg);margin-right:10px;color:white"></i><span>{{company_info.company_tel}}</span></a>
-                <a class="info_container__whatsapp"><i class="fa fa-whatsapp" style="color:white"></i></a>
-            </div>
-            <div>
-                <a class="info_container__addrBtn">
-                    <i class="fa fa-map-marker" style="color:black;margin-right:10px"></i>
-                    <span>{{company_info.company_address}}</span>
-                </a>
-            </div>
-        </div>
-        <div class="info_images">
-            <div class="info_images__item" v-for="(data,index) in products" :key="index">
-                <img :src="data.image">
-            </div>
-        </div>
-     
-	</div>
+  <div class="info">
+    <div class="info_container">
+      <div
+        class="info_container__img"
+        :style="{'background-image': `url(.${company_info.company_image})`}"
+      ></div>
+      <h3 class="company_name">{{company_info.company_name}}</h3>
+      <h5 class="company_address">{{company_info.company_address}}</h5>
+    </div>
+    <div class="info_btns">
+      <div class="info_container__btn">
+        <a class="info_container__numb info_btn_black noSelect" :href="`tel:${company_info.company_tel}`">
+          <i class="fa 2x fa-phone" style="transform:rotate(90deg);margin-right:10px;color:white"></i>
+          <span>{{company_info.company_tel}}</span>
+        </a>
+        <a
+          class="info_container__whatsapp noSelect"
+          :href="`https://api.whatsapp.com/send?phone=${company_info.company_wp_tel}&text=${getUrlMessageText(company_info.company_wp_message_text)}`"
+        >
+          <i class="fa fa-whatsapp" style="color:white"></i>
+        </a>
+      </div>
+      <div>
+        <a
+          class="info_container__addrBtn noSelect"
+          :href="`geo:${company_info.company_geo.lat},${company_info.company_geo.long};u=35`"
+        >
+          <i class="fa fa-map-marker" style="color:black;margin-right:10px"></i>
+          <span>{{company_info.company_address}}</span>
+        </a>
+      </div>
+    </div>
+    <div class="info_images">
+      <div class="info_images__item" v-for="(data,index) in products" :key="index">
+        <img :src="data.image" />
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-	export default{
-		name:'info',
-		data () {
-        return {
-            title: 'Info',
-            company_info    :   {
-                company_name : 'AlmaGold',
-                company_address :   'Г.АЛМАТЫ, УЛ. АБАЯ 1',
-                company_tel : '+7 777 777 77 77',
-                company_image : require('../assets/company_icon/company_icon.svg'),
-                company_tel_icon : require('../assets/company_icon/company_tel_icon.svg')
-            },
-            products:[
-            {
-                productTitle:"ABCN",
-                image       : require('../assets/images/product1.png'),
-                productId:1
-            },
-            {
-                productTitle:"KARMA",
-                image       : require('../assets/images/product2.png'),
-                productId:2
-            },
-            {
-                productTitle:"Tino",
-                image       : require('../assets/images/product3.png'),
-                productId:3
-            },
-            {
-                productTitle:"EFG",
-                image       : require('../assets/images/product4.png'),
-                productId:4
-            },
-            {
-                productTitle:"MLI",
-                image       : require('../assets/images/product5.png'),
-                productId:5
-            },
-            {
-                productTitle:"Banans",
-                image       : require('../assets/images/product6.png'),
-                productId:6
-            }
-            ] 
-        }
+export default {
+  name: "info",
+  data() {
+    return {
+      title: "Info",
+      company_info: {
+        company_name: "AlmaGold",
+        company_address: "Г.АЛМАТЫ, УЛ. АБАЯ 1",
+        company_wp_tel: "77074218438",
+        company_wp_message_text:
+          "Я заинтересован(-а) в покупке вашего товара на сайте OnlainVitrina",
+        company_tel: "+7 777 777 77 77",
+        company_geo: { lat: "43.238949", long: "76.889709" },
+        company_image: require("../assets/company_icon/company_icon.svg"),
+        company_tel_icon: require("../assets/company_icon/company_tel_icon.svg"),
+      },
+      products: [
+        {
+          productTitle: "ABCN",
+          image: require("../assets/images/product1.png"),
+          productId: 1,
+        },
+        {
+          productTitle: "KARMA",
+          image: require("../assets/images/product2.png"),
+          productId: 2,
+        },
+        {
+          productTitle: "Tino",
+          image: require("../assets/images/product3.png"),
+          productId: 3,
+        },
+        {
+          productTitle: "EFG",
+          image: require("../assets/images/product4.png"),
+          productId: 4,
+        },
+        {
+          productTitle: "MLI",
+          image: require("../assets/images/product5.png"),
+          productId: 5,
+        },
+        {
+          productTitle: "Banans",
+          image: require("../assets/images/product6.png"),
+          productId: 6,
+        },
+      ],
+    };
+  },
+  methods: {
+    getUrlMessageText(text) {
+      text.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, "");
+      return text.replace(" ", "%20");
     },
-	}
+  },
+};
 </script>
     
 <style scoped>
-    .info{
-        margin : 0px 20px;
-    }
-    .info_container{
-        width : 100%;
-        height : 300px;
-        text-align:center
-    }
-    .info_container__img{
-        margin-left:auto;
-        margin-right:auto;
-        margin-top:50px;
-        margin-bottom:auto;
-        height: 50%;
-        width: 150px;
-        background-size: cover;
-        background-position: center;
-    }
-    .company_name{
-        font-family: 'Comfortaa';
-        font-style: normal;
-        font-weight: bold;
-        font-size: 36px;
-        line-height: 40px;
-        letter-spacing: -0.015em;
-        margin-bottom:10px;
-    }
-    .company_address{
-        font-family: 'Roboto';
-        font-style: normal;
-        font-weight: 900;
-        font-size: 13px;
-        line-height: 15px;
-        letter-spacing: 0.04em;
-        text-transform: uppercase;
-        margin-top:5px;
-    }
-    .info_container__btn{
-        width:100%;
-        justify-content: space-between;
-        display: flex;
-    }
-    .info_container__whatsapp{
-        display:block;
-        display:flex;
-        background: #000000;
-        border: 2px solid #000000;
-        box-sizing: border-box;
-        border-radius: 6px;
-        font-family: 'Roboto';
-        font-style: normal;
-        font-weight: 900;
-        font-size: 13px;
-        line-height: 15px;
-        letter-spacing: 0.04em;
-        text-transform: uppercase;
-        width:25%;
-        height:44px;
-        color: #FFFFFF;
-        align-items: center;
-        justify-content: center;
-    }
-    .info_container__numb{
-        display:block;
-        display:flex;
-        background: #000000;
-        border: 2px solid #000000;
-        box-sizing: border-box;
-        border-radius: 6px;
-        font-family: 'Roboto';
-        font-style: normal;
-        font-weight: 900;
-        font-size: 13px;
-        line-height: 15px;
-        letter-spacing: 0.04em;
-        text-transform: uppercase;
-        width:70%;
-        height:44px;
-        color: #FFFFFF;
-        align-items: center;
-        justify-content: center;
-    }
-    .info_container__addrBtn{
-        display:block;
-        display:flex;
-        width:100%;
-        height:44px;
-        text-align:center;
-        margin-top:20px;
-        background: #FFFFFF;
-        border: 2px solid #000000;
-        box-sizing: border-box;
-        border-radius: 6px;
-        font-family: Roboto;
-        font-style: normal;
-        font-weight: 900;
-        font-size: 13px;
-        line-height: 15px;
-        letter-spacing: 0.04em;
-        text-transform: uppercase;
-        align-items: center;
-        justify-content: center;
-    }
-    .info_images{
-        display: flex;
-        flex-wrap: wrap;
-        padding: 16px 0;
-    }
-    .info_images__item{
-        max-height:60vh;
-        overflow: hidden;
-        display:flex;
-        width: 100%;
-        flex: 0 48% ;
-        padding-bottom: 10px;
-    }
-   .info_images__item:nth-child(2n+1){
-       margin-right: 10px;
-   }
-    .info_images__item img{
-        margin-left: -50%;
-        min-height: 100% !important;
 
-    }
-    
+.info {
+  margin: 0px 20px;
+}
+.info_container {
+  width: 100%;
+  height: 300px;
+  text-align: center;
+}
+.info_container__img {
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 50px;
+  margin-bottom: auto;
+  height: 50%;
+  width: 150px;
+  background-size: cover;
+  background-position: center;
+}
+.info_btns a {
+  color: #000;
+  text-decoration: none;
+}
+.info_btns a:visited {
+  color: #000;
+}
+a.info_btn_black {
+  color: #fff;
+}
+a:visited.info_btn_black {
+  color: #fff;
+}
+
+.company_name {
+  font-family: "Comfortaa";
+  font-style: normal;
+  font-weight: bold;
+  font-size: 36px;
+  line-height: 40px;
+  letter-spacing: -0.015em;
+  margin-bottom: 10px;
+}
+.company_address {
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 900;
+  font-size: 13px;
+  line-height: 15px;
+  letter-spacing: 0.04em;
+  text-transform: uppercase;
+  margin-top: 5px;
+}
+.info_container__btn {
+  width: 100%;
+  justify-content: space-between;
+  display: flex;
+}
+.info_container__whatsapp {
+  display: block;
+  display: flex;
+  background: #000000;
+  border: 2px solid #000000;
+  box-sizing: border-box;
+  border-radius: 6px;
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 900;
+  font-size: 13px;
+  line-height: 15px;
+  letter-spacing: 0.04em;
+  text-transform: uppercase;
+  width: 44px;
+  height: 44px;
+  color: #ffffff;
+  align-items: center;
+  justify-content: center;
+}
+.info_container__whatsapp i {
+  font-size: 20px;
+}
+
+.info_container__numb {
+  display: flex;
+  background: #000000;
+  border: 2px solid #000000;
+  box-sizing: border-box;
+  border-radius: 6px;
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 900;
+  font-size: 13px;
+  line-height: 15px;
+  letter-spacing: 0.04em;
+  text-transform: uppercase;
+  height: 44px;
+  color: #ffffff;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  margin-right: 10px;
+}
+.info_container__addrBtn {
+  display: block;
+  display: flex;
+  width: 100%;
+  height: 44px;
+  text-align: center;
+  margin-top: 20px;
+  background: #ffffff;
+  border: 2px solid #000000;
+  box-sizing: border-box;
+  border-radius: 6px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 900;
+  font-size: 13px;
+  line-height: 15px;
+  letter-spacing: 0.04em;
+  text-transform: uppercase;
+  align-items: center;
+  justify-content: center;
+}
+.info_images {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 16px 0;
+}
+.info_images__item {
+  max-height: 60vh;
+  overflow: hidden;
+  display: flex;
+  width: 100%;
+  flex: 0 48%;
+  padding-bottom: 10px;
+}
+.info_images__item:nth-child(2n + 1) {
+  margin-right: 10px;
+}
+.info_images__item img {
+  margin-left: -50%;
+  min-height: 100% !important;
+}
 </style>
